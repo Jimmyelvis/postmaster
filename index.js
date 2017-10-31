16f701a879ac15852b157f4675aa4781ada7f22f
Mark <dylancougar@yahoo.com>
2017-10-31 19:26:50 -0400

    changed greeting

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'mannnn' });
+  res.send({ hi: 'fooollllllllllllll' });
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
-//https://chilling-witch-52579.herokuapp.com/ | https://git.heroku.com/chilling-witch-52579.git
+//https://gruesome-grave-12486.herokuapp.com/ | https://git.heroku.com/gruesome-grave-12486.git
