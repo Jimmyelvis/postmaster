60701804d766877fb5eb6c0f6fff0cb2b47082cb
Jimmyelvis <jinjoe@zoho.com>
2018-07-17 19:11:44 -0500

    Updated with instructions

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -13,6 +13,14 @@ const Dashboard = () => {
 
           <div className="surveylist container">
 
+            <div className="readme" id="readme">
+
+              <p>To send out a YES/NO survey, please click the plus button on the
+            bottom right. For your list of recipients, simply enter as many valid emails as you want separated by a comma, no spaces. When a completed survey is successfuly sent. It will be
+            shown here on  your dashboard.</p>
+
+            </div>
+
              <SurveyList />
 
               <div className="fixed-action-btn">
