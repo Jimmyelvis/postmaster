6a62a1c8fe1171caee172fef75ff4e58d044a9e8
Jimmyelvis <jinjoe@zoho.com>
2021-03-16 17:49:30 -0400

    updated index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,37 +1,34 @@
 // import 'materialize-css/dist/css/materialize.min.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import reduxThunk from 'redux-thunk';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import reduxThunk from "redux-thunk";
 
-
-import App from './components/App';
-import reducers from './reducers';
+import App from "./components/App";
+import reducers from "./reducers";
 
 // Development only axios helpers!
-import axios from 'axios';
+import axios from "axios";
 window.axios = axios;
 
 const middleware = [reduxThunk];
 
-
-
 const store = createStore(
-  reducers, 
-  {}, 
+  reducers,
+  {},
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({trace: true})
+    applyMiddleware(...middleware)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({trace: true})
   )
 );
 
-
 ReactDOM.render(
-  <Provider store={store}><App /></Provider>,
-  document.querySelector('#root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
 );
 
-
-console.log('Stripe key is', process.env.REACT_APP_STRIPE_KEY);
-console.log('Enviroment is', process.env.NODE_ENV);
+console.log("Stripe key is", process.env.REACT_APP_STRIPE_KEY);
+console.log("Enviroment is", process.env.NODE_ENV);
