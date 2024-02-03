7e2f4803baa007f9043f80e983b73956a4dc1783
Jimmyelvis <32781044+Jimmyelvis@users.noreply.github.com>
2024-02-03 15:40:32 -0500

    Delete client/src/actions directory

--- client/src/actions/types.js
+++
@@ -1,9 +0,0 @@
-export const FETCH_USER = "fetch_user";
-export const FETCH_SURVEYS = "fetch_surveys";
-export const GET_ERRORS = "get_errors";
-export const CLEAR_ERRORS = "clear_errors";
-export const FETCH_SURVEY = "fetch_survey";
-export const SORT_SURVEYS_TITLE_ASC = "sort_survey_title_asc";
-export const SORT_SURVEYS_TITLE_DESC = "sort_survey_title_desc";
-export const SORT_SURVEYS_DATE_ASC = "sort_survey_date_asc";
-export const SORT_SURVEYS_DATE_DESC = "sort_survey_date_desc";
