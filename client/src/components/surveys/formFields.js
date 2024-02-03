a9d020b12002d9b63665c524a3aae77a60dd0d5c
Jimmyelvis <32781044+Jimmyelvis@users.noreply.github.com>
2024-02-03 15:52:44 -0500

    Delete client/src/components/surveys directory

--- client/src/components/surveys/formFields.js
+++
@@ -1,9 +0,0 @@
-export default [
-
-  { label: 'Survey Title', name: 'title', noValueError: 'Provide a Survey Title' },
-  { label: 'Subject', name: 'subject', noValueError: 'Provide a Subject for your survey' },
-  { label: 'Company Email', name: 'company', noValueError: 'You need to provide ypur companies email' },
-  { label: 'Email Body', name: 'body', noValueError: 'Your survey is going to need a body' },
-  { label: 'Recipient List', name: 'recipients' , noValueError: 'You Need a list of email receivers'}
-
-];
